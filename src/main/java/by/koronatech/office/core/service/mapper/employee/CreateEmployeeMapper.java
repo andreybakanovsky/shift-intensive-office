8649a7e4dc473b1e46181f97e90b3ff4ec8d506f
Andrey Bakanovsky <andrey.bakanovsky@outlook.com>
2025-03-03 14:00:33 +0300

    Change CreateEmployeeMapper

--- src/main/java/by/koronatech/office/core/service/mapper/employee/CreateEmployeeMapper.java
+++ src/main/java/by/koronatech/office/core/service/mapper/employee/CreateEmployeeMapper.java
@@ -1,6 +1,7 @@
 package by.koronatech.office.core.service.mapper.employee;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import by.koronatech.office.api.dto.employee.CreateEmployeeDTO;
 import by.koronatech.office.core.entity.Employee;
@@ -8,4 +9,8 @@ import by.koronatech.office.core.service.mapper.BaseMapper;
 
 @Mapper(config = BaseMapper.class)
 public interface CreateEmployeeMapper extends BaseMapper<Employee, CreateEmployeeDTO> {
+
+    @Override
+    @Mapping(source = "departmentId", target = "department.id")
+    Employee toEntity(CreateEmployeeDTO createEmployeeDTO);
 }
